f2cef76a6eae6e9629e449174ef2361f76029012
nilkun <nilkunsoftware@gmail.com>
2018-10-23 11:23:48 +0900

    Updates

---
+++ precache-manifest.cb86a8b5e3ed1fa95bb10066516b1f16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "0937fb361383057a30a3",
+    "url": "/robofriends/static/js/main.0937fb36.chunk.js"
+  },
+  {
+    "revision": "abeff98438bb0aab8850",
+    "url": "/robofriends/static/js/1.abeff984.chunk.js"
+  },
+  {
+    "revision": "0937fb361383057a30a3",
+    "url": "/robofriends/static/css/main.a6c72763.chunk.css"
+  },
+  {
+    "revision": "abeff98438bb0aab8850",
+    "url": "/robofriends/static/css/1.863a4a37.chunk.css"
+  },
+  {
+    "revision": "52406895bb1d9a30a203d534f627400c",
+    "url": "/robofriends/index.html"
+  }
+];
