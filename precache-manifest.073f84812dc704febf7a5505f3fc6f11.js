393a14f91336c023661a8810743a9ffd3d9545b5
nilkun <nilkunsoftware@gmail.com>
2018-10-23 11:35:34 +0900

    Updates

--- precache-manifest.073f84812dc704febf7a5505f3fc6f11.js
+++ precache-manifest.073f84812dc704febf7a5505f3fc6f11.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
   },
   {
-    "revision": "0937fb361383057a30a3",
-    "url": "/robofriends/static/js/main.0937fb36.chunk.js"
+    "revision": "4565ad483a8969ca5593",
+    "url": "/robofriends/static/js/main.4565ad48.chunk.js"
   },
   {
     "revision": "abeff98438bb0aab8850",
     "url": "/robofriends/static/js/1.abeff984.chunk.js"
   },
   {
-    "revision": "0937fb361383057a30a3",
+    "revision": "4565ad483a8969ca5593",
     "url": "/robofriends/static/css/main.a6c72763.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/robofriends/static/css/1.863a4a37.chunk.css"
   },
   {
-    "revision": "52406895bb1d9a30a203d534f627400c",
+    "revision": "bb2cd8e52729c0b288081f475964810b",
     "url": "/robofriends/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.cb86a8b5e3ed1fa95bb10066516b1f16.js"
+  "/robofriends/precache-manifest.073f84812dc704febf7a5505f3fc6f11.js"
 );
 
 workbox.clientsClaim();
